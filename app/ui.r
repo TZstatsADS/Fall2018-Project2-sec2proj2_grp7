6c67135537abf0fa5b45998f164c67f7ee3ea89f
Rui Zhang <rz2406@columbia.edu>
2018-10-04 19:32:34 -0400

    Summay Plotly

--- app/ui.r
+++ app/ui.r
@@ -2,60 +2,25 @@ library(shiny)
 library(leaflet)
 library(shinydashboard)
 header <- dashboardHeader(
-  title = "Twin Cities Buses"
+  title = "Hospital Recomendation"
 )
 body <- dashboardBody(
-  fluidRow(
-    column(width = 9,
-           box(width = NULL, solidHeader = TRUE,
-               leafletOutput("busmap", height = 500)
-           ),
-           box(width = NULL,
-               uiOutput("numVehiclesTable")
-           )
-    ),
-    column(width = 3,
-           box(width = NULL, status = "warning",
-               uiOutput("routeSelect"),
-               checkboxGroupInput("directions", "Show",
-                                  choices = c(
-                                    Northbound = 4,
-                                    Southbound = 1,
-                                    Eastbound = 2,
-                                    Westbound = 3
-                                  ),
-                                  selected = c(1, 2, 3, 4)
+  dropdownMenu(type = "tasks", badgeStatus = "success",
+               taskItem(value = 90, color = "green",
+                        "Documentation"
                ),
-               p(
-                 class = "text-muted",
-                 paste("Note: a route number can have several different trips, each",
-                       "with a different path. Only the most commonly-used path will",
-                       "be displayed on the map."
-                 )
+               taskItem(value = 17, color = "aqua",
+                        "Project X"
                ),
-               actionButton("zoomButton", "Zoom to fit buses")
-           ),
-           box(width = NULL, status = "warning",
-               selectInput("interval", "Sort",
-                           choices = c(
-                             "State" = 30,
-                             "DRG" = 60,
-                             "Cost" = 120,
-                             "Quality Score" = 300,
-                             "Preference" = 600
-                           ),
-                           selected = "60"
+               taskItem(value = 75, color = "yellow",
+                        "Server deployment"
                ),
-               uiOutput("timeSinceLastUpdate"),
-               actionButton("refresh", "Refresh now"),
-               p(class = "text-muted",
-                 br(),
-                 "Source data updates every 30 seconds."
+               taskItem(value = 80, color = "red",
+                        "Overall project"
                )
-           )
-    )
+  ) 
+    
   )
-)
 
 dashboardPage(
   header,
