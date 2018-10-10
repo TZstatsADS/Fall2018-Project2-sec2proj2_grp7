161980b4ff68c9b2b57244f4311d989c196251df
lujia wang <lw2772@columbia.edu>
2018-10-10 15:57:41 -0400

    app modify - finalize on recommendation

--- app/server.R
+++ app/server.R
@@ -14,22 +14,24 @@ server <- function(input, output) {
   })
   # filter data with state
   df1 <- reactive({
-    if (r.state() == "Select") {
-      df1 <- hospital_info
-    }
-    else {
+    
       df1 <- hospital_info %>% filter(State == r.state())
-    }
+    
   })
   # filter data with selected DRG group
   df2 <- reactive({
-    if (r.drg() == "Select") {
-      df2 <- df1()
-    }
-    else{
+   
       df2 <- df1() %>% filter(drg == r.drg())
-    }
+    
   })
+  # filter data with cost range
+  df3 <- reactive({
+    df3 <- df2() %>% filter(Average.Total.Payments >= r.cost()[1] &
+                                             Average.Total.Payments <= r.cost()[2]) 
+  })
+  
+  
+  
   # reweight with selected most care aspect
   # Calculate care vector
   o.c.w <- c(1, 1, 1, 1, 1, 1, 1)
@@ -53,7 +55,7 @@ server <- function(input, output) {
   
   # Scores of hospitals in the selected state
   score <-
-    reactive(apply(data.frame(df2()), 1, getscore, care.w = c.weight()))
+    reactive(apply(data.frame(df3()), 1, getscore, care.w = c.weight()))
   
   # orders for hospitals
   ord <- reactive(order(score(), decreasing = TRUE))
@@ -64,23 +66,20 @@ server <- function(input, output) {
       score(), order = -1, ties.method = "min"
     )))
   #Reactive expression for the map/recommendation data subsetted to what the user selected
-  df3 <- reactive(cbind(df2(), Order = ord(), Rank = rk()))
+  df4 <- reactive(cbind(df3(), Order = ord(), Rank = rk()))
   
   # Outputs
   output$r.df = renderDataTable({
     rankedtable <-
       DT::datatable(
-        cbind(df3()$Rank[ord()], df3()[ord(), c("Hospital.Name", "Address.x", "City",
-                                                "State")]),
+        #cbind(df4()$Rank[ord()], df4()[ord(), c("Hospital.Name")]),
+        df4()[ord(),c("Rank","Hospital.Name","Address")],
         selection = "single",
-        options = list(stateSave = TRUE)
+        options = list(stateSave = TRUE,pageLength=5,lengthMenu=c(5,10,15,20)),rownames = FALSE
       )
     rankedtable
-  }, options = list(
-    orderClasses = TRUE,
-    pageLength = 5,
-    lengthMenu = c(5, 10, 15, 20)
-  ))
+  }
+  )
   # to keep track of previously selected row
   prev_row <- reactiveVal()
   # new icon style
@@ -90,13 +89,13 @@ server <- function(input, output) {
                     iconColor = "white")
   # Observe event - select row in table
   observeEvent(input$r.df_rows_selected, {
-    row_selected = df3()[input$r.df_rows_selected, ]
+    row_selected = df4()[input$r.df_rows_selected, ]
     proxy <- leafletProxy('map')
     proxy %>% addAwesomeMarkers(
       lng = row_selected$lon,
       lat = row_selected$lat,
       icon = icon.flag,
-      layerId = as.character(row_selected$Provider.Id),
+      layerId = as.character(row_selected$Provider.ID),
       popup = content.fun(row_selected)
     ) %>% fitBounds(
       lng1 = row_selected$lon + 0.1,
@@ -109,7 +108,7 @@ server <- function(input, output) {
       proxy %>% addMarkers(
         lng = prev_row()$lon,
         lat = prev_row()$lat,
-        layerId = as.character(prev_row()$Provider.Id),
+        layerId = as.character(prev_row()$Provider.ID),
         popup = content.fun(prev_row())
       )
     }
@@ -119,27 +118,27 @@ server <- function(input, output) {
   
   #leaflet map output (interactive with user's input)
   output$map <- renderLeaflet({
-    leaflet(data = df3()) %>% addTiles() %>% addAwesomeMarkers(
+    leaflet(data = df4()) %>% addTiles() %>% addAwesomeMarkers(
       lng = ~ lon,
       lat = ~ lat,
-      popup = content.fun(df3()),
-      layerId = as.character(df3()$Provider.Id)
+      popup = content.fun(df4()),
+      layerId = as.character(df4()$Provider.ID)
     )
   })
   # observe Event - click on map
   observeEvent(input$map_marker_click, {
-    clickId <- input$map_marker_click$Provider.Id
+    clickId <- input$map_marker_click$Provider.ID
     dataTableProxy("r.df") %>%
-      selectRows(which(df3()$Provider.Id == clickId)) %>%
+      selectRows(which(df4()$Provider.ID == clickId)) %>%
       selectPage(which(input$r.df_rows_all == clickId) %/%
                    input$r.df_state$length + 1)
   })
-  
+
   #value box - number of hospitals
   output$vbox_1 <- renderValueBox({
     valueBox(
       subtitle = "Number of Hospitals",
-      value = nrow(df3()),
+      value = nrow(df4()),
       color = "purple",
       icon = icon("list")
     )
@@ -160,7 +159,7 @@ server <- function(input, output) {
     valueBox(
       subtitle = "Average Cost",
       value = paste(round(
-        mean(df3()$Average.Total.Payments, na.rm = TRUE), 0
+        mean(df4()$Average.Total.Payments, na.rm = TRUE), 0
       ), "$"),
       color = "blue",
       icon = icon("credit-card")
@@ -168,3 +167,4 @@ server <- function(input, output) {
   })
   
 }
+
