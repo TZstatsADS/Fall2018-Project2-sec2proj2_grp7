4726e008b60fa96b2be7f3c4460ae199e0ac7cc5
Xinwei Feng <xf2168@columbia.edu>
2018-10-10 18:51:02 -0400

    change

--- app/server.R
+++ app/server.R
@@ -10,37 +10,36 @@ server <- function(input, output) {
   
   output$read2 <-
     renderText({
-      "-> Step 1: Select the state you live in or you wish to go to and the cost range you are comfortable to pay."
+      " •	Step 1: Select the state you live in or you wish to go to and the cost range you are comfortable to pay."
     })
   output$read3 <-
     renderText({
-      "-> Step 2: Choose one of the most cared hospital qualities and your diagnosis related group."
+      " •	Step 2: Choose one of the most cared hospital qualities and your diagnosis related group."
     })
   output$read4 <-
     renderText({
-      "-> Step 3: Check the search result table and the map for the basic information of all hospitals."
+      " •	Step 3: Check the search result table and the map for the basic information of all hospitals."
     })
   output$read5 <-
     renderText({
-      "-> Step 4: Click one you are interested in from the table to zoom it in on the map to see the exact location of the hospital."
+      " •	Step 4: Click one you are interested in from the table to zoom it in on the map to see the exact location of the hospital."
     })
   
   output$read6 <-
     renderText({
-      "Want more information about spending? Please click the Overview tab to explore more!"
-    })
+      "Want more information about spending? Please click the Overview tab to explore more!"})
   
   output$read7 <-
     renderText({
-      "Part 1: Check the State Overview to get a overview of average charges and average medicare payments by states"
+      "Part 1: Check the State Overview to get a overview of average charges and average medicare payments by states."
     })
   output$read8 <-
     renderText({
-      "-> Select the year and diagnosis related group you are interested in.You can also search one state to see the detailed charges and payments inforamtion."
+      "  • Select the year and diagnosis related group you are interested in.You can also search one state to see the detailed charges and payments inforamtion."
     })
   output$read9 <-
     renderText({
-      "Part 2: Take a look at our Hospital Comparison by selecting year, DRG, and the hospitals you are cared about"
+      "Part 2: Take a look at our Hospital Comparison by selecting year, DRG, and the hospitals you are cared about."
     })
   output$read10 <-
     renderText({
@@ -55,7 +54,7 @@ server <- function(input, output) {
   })
   output$team1 <-
     renderText({
-      "We are five excellent students from Columbia University in Statistics and the owners of the Version 2.0 medicare app. We aim to make data more straightforward and convinent for medicare searching.)"
+      "We are five excellent students from Columbia University in Statistics and the owners of the Version 2.0 medicare app. We aim to make data more straightforward and convinent for medicare searching."
     })
   output$team2 <-
     renderText({
@@ -63,23 +62,23 @@ server <- function(input, output) {
     })
   output$team3 <-
     renderText({
-      "-> Xinwei Feng (email: xf2168@columbia.edu)"
+      "•	Xinwei Feng (email: xf2168@columbia.edu)"
     })
   output$team4 <-
     renderText({
-      "-> Lujia Wang (email: lw2772@columbia.edu)"
+      "•	Lujia Wang (email: lw2772@columbia.edu)"
     })
   output$team5 <-
     renderText({
-      "-> Binhan Wang (email: bw2544@columbia.edu)"
+      "•	Binhan Wang (email: bw2544@columbia.edu)"
     })
   output$team6 <-
     renderText({
-      "-> Rui Zhang (email: rz2406@columbia.edu)"
+      "•	Rui Zhang (email: rz2406@columbia.edu)"
     })
   output$team7 <-
     renderText({
-      "-> Yixin Zhang (email: yz3223@columbia.edu)"
+      "•	Yixin Zhang (email: yz3223@columbia.edu)"
     })
   output$team8 <- renderText({
     "Thank you for using!"
